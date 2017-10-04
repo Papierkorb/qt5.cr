a570e6918fd5d3959c3e939b9076afb5c0a18b42
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-10-04 21:49:02 +0200

    Use QT_VERSION_* from linked library

--- src/qt5/application.cr
+++ src/qt5/application.cr
@@ -4,10 +4,7 @@ module Qt
     @argc : Int32 = ARGC_UNSAFE
 
     def initialize
-      # The `0x50901` magic refers to the Qt library that was linked against.
-      # It's a version identifier.  Even better, this argument is completely
-      # undocumented :)
-      @unwrap = Binding.bg_QApplication_CONSTRUCT_int_R_char_XX_int(pointerof(@argc), @argv, 0x50901)
+      @unwrap = Binding.bg_QApplication__CONSTRUCT_int_R_char_XX_int(pointerof(@argc), @argv, QT_VERSION)
     end
   end
 end
--- src/qt5/version.cr
+++ src/qt5/version.cr
@@ -1,6 +1,8 @@
 module Qt
   VERSION = "0.1.0"
 
+  QT_VERSION = (QT_VERSION_MAJOR << 16) | (QT_VERSION_MINOR << 8) | QT_VERSION_PATCH
+
   # Run-time version of the used Qt libraries.  The version may differ from the
   # version used at link-time.  See `Qt::QT_VERSION_STR` for the link-time
   # version.
