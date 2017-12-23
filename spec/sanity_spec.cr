1d9e31c0f4816ca1f9d207071ba16a5aeb22a60e
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-12-23 23:40:38 +0100

    spec/sanity_spec.cr: Print Crystal version

--- spec/sanity_spec.cr
+++ spec/sanity_spec.cr
@@ -4,6 +4,6 @@ describe "Qt bindings" do
   it "works" do
     # If we even get here, it means the bindings are sane enough to link on this
     # system.  There's a good chance other things will work too.
-    puts "Qt version: #{Qt.version}, linked against #{Qt::QT_VERSION_STR}"
+    puts "Qt #{Qt.version}, linked to #{Qt::QT_VERSION_STR} / Crystal #{Crystal::VERSION}"
   end
 end
